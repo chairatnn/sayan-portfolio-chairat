7f9368d8b1c47de6e6ea83f023e515de9a0d6ab6
chairatnn <61372139+chairatnn@users.noreply.github.com>
2025-12-21 13:40:12 +0700

    fix-next-config-mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     // เพิ่มส่วนนี้เพื่อบังคับให้ Next.js รับรู้ค่าตัวแปร
+  env: {
+    NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
+    NEXT_PUBLIC_GITHUB_USERNAME: process.env.NEXT_PUBLIC_GITHUB_USERNAME,
+  },
+  // การตั้งค่าอื่นๆ...
+};
 
 export default nextConfig;
